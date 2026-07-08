bab3419879b4033608d798cc68ed960dc99a7cb3
Nehil Patel <158016904+nehilonkeys@users.noreply.github.com>
2026-07-08 12:35:03 -0500

    Clean up comments and retain print statement

--- main.py
+++ main.py
@@ -539,10 +539,3 @@ time = 0
 
 # total = principal * pow(( 1 + (rate/100 )), time)
 # print(f"Balance after {time} year/s: ${total:.2f}")
-
-# for loops = execute a block of code a fixed number of times.
-            #   You can interate over a range, string, sequence, etc.
-
-for x in range(1, 11):
-    print(x)
-    
